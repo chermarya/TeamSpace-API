75db3c866f19e148541696ae3d854c3e1dd98699
chermarya <chermarya25@gmail.com>
2024-11-02 14:39:55 +0200

    test DB work

---
+++ TeamSpace-API/TeamSpace-API/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TeamSpace_API.Data;
+using System.Linq;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public UserController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetUsers()
+    {
+        var users = _context.Users.ToList();
+        return Ok(users);
+    }
+}
